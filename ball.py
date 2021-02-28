16ab4b421ca024c3983cad32d22d1197dc343919
Yochi Hsu <hs41511@gmail.com>
2021-02-28 16:40:29 +0800

    color change

--- ball.py
+++ ball.py
@@ -4,11 +4,13 @@ from vector_operations import *
 screen_width = 420
 screen_height = 540
 
+size = (20,20)
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, pos, v, pic_path):
         super().__init__()
-        self.image = pygame.image.load(pic_path)
+        self.image = pygame.Surface(size)
+        pygame.draw.circle(self.image, (255,255,255), (10,10), 10)
         self.rect = self.image.get_rect()
         self.rect.center = (round(pos[0]), round(pos[1]))
         self.pos = pos
@@ -18,6 +20,7 @@ class Ball(pygame.sprite.Sprite):
         self.collided = []
 
     def update(self):
+        
         self.pos = (self.pos[0]+self.v[0], self.pos[1]+self.v[1])
         self.rect.center = (round(self.pos[0]), round(self.pos[1]))
 
